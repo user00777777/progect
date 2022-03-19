f9b09ee746d34452715f4b5ba61d01815b212f77
user00777777 <97362584+user00777777@users.noreply.github.com>
2022-03-19 09:50:22 +0200

    js
    js

--- work.js
+++ work.js
@@ -5,7 +5,10 @@ let malko=[1,2.2,3,4,5,6,7,8,9,10,11,12,13,14,15,18,19.2,22,23.2,26,27.2,30,31.2
 let kryglui=[1,2.2,3,4,5,6,7,8,9,10,11,12,13,14,15,16.2,19,20.2,23,24.2,27,28.2,31]
 let sinkevich=[1,2.2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17.2,20,21.2,24,25.2,26,27.2,30,31.2]
 let out=document.querySelector('.out');
-let out2=document.querySelector('.out2')
+let out2=document.querySelector('.out2');
+let out2=document.querySelector('.out3');
+let out2=document.querySelector('.out4');
+
 
 
 
@@ -38,12 +41,12 @@ switch (vital,stas,paha,vasj) {
    case day2:if (hour>=19&&stas==day2) {out2.innerHTML='Дежурный электрик Круглый 2 смена<br>Дежурный сантехник Головченко С.';//out2.style.add='out3'
       
    }
-   case day:if (hour>=7&&hour<19&&day==stas) {out3.innerHTML='Круглый 1 смена <br>Дежурный сантехник Головченко С.';}
+   case day:if (hour>=7&&hour<19&&day==stas) {out3.innerHTML='Дежурный электрик Круглый 1 смена <br>Дежурный сантехник Головченко С.';}
    case day:if(hour>=0&&hour<19&&day==paha)  {out2.innerHTML='Дежурный электрик Лысенко П. 1 смена <br>Дежурный сантехник Немченко В.';     };
 
    
-   case  day2: if (hour>=19&&day2==paha) out.innerHTML='Лысенко  2 смена <br>Дежурный сантехник Немченко В.';
-   case day:if(hour>=7&&hour<19&&day==vasj)  {   out.innerHTML='Малько 1 смена <br>Дежурный сантехник Ничипорчук А.'};
+   case  day2: if (hour>=19&&day2==paha) out.innerHTML=' Дежурный электрик Лысенко  2 смена <br>Дежурный сантехник Немченко В.';
+   case day:if(hour>=7&&hour<19&&day==vasj)  {   out.innerHTML='Дежурный электрик Малько 1 смена <br>Дежурный сантехник Ничипорчук А.'};
 
    
    case  day2: if (hour>=19&&day2==vasj) out.innerHTML='Малько  2 смена <br>Дежурный сантехник Ничипорчук А';
