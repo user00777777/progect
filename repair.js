5827ceebbbdbdf91eb2283b550420005a756f26b
user00777777 <misterborman@gmail.com>
2022-06-19 23:51:48 +0300

    тельфера

--- repair.js
+++ repair.js
@@ -4,14 +4,35 @@ var month = today.getMonth() + 1;
 var year = today.getFullYear();
 let data = `${year}${month}${day}  `;
 console.log(year);
-
+let dates = [{ datesYear: [2022, 2023, 2024, 2025] }];
 let tel795 = document.getElementById("telf_795");
+let tel798 = document.querySelector("#telf_798");
 tel795.addEventListener("click", telChange);
+tel798.addEventListener("click", telChange798);
+
+function telChange798() {
+  tel798.setAttribute("id", "t");
+  let result = (m) => {
+    let rr;
+    switch (year) {
+      case 2022:
+        rr = m + 6 - 12;
+        break;
+
+      default:
+        break;
+    }
+    return rr;
+  };
+  let r = result(month);
+  tel798.innerHTML = `тельфер не поцеплен упругое тело заменено(резиновая муфта)`;
+}
+
 function telChange() {
   const oldDay = 9;
   const oldMonth = 6;
   const oldYear = 2022;
-  tel795.setAttribute("class", "t");
+  tel795.setAttribute("id", "t");
 
   function count(month) {
     let resultMonth;
