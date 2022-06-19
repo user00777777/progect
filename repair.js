39b0b7a2118101e46dc3b7901864fe93f6525000
user00777777 <misterborman@gmail.com>
2022-06-19 19:32:55 +0300

    count

--- repair.js
+++ repair.js
@@ -3,22 +3,42 @@ var day = today.getDate();
 var month = today.getMonth() + 1;
 var year = today.getFullYear();
 let data = `${year}${month}${day}  `;
-console.log(data);
-const oldDay = 9;
-const oldMonth = 6;
-const oldYear = 2022;
+console.log(year);
 
-function countMonth(month, oldMonth) {
-  let resultMonth = month + oldMonth - 12;
-  return resultMonth;
-}
-let r = countMonth(month, oldMonth);
-
-console.log(r);
 let tel795 = document.getElementById("telf_795");
 tel795.addEventListener("click", telChange);
 function telChange() {
-  tel795.innerHTML = `Замена полумуфты на двигателе 9.06.22 прошло ${r}месяцев `;
-
+  const oldDay = 9;
+  const oldMonth = 6;
+  const oldYear = 2022;
   tel795.setAttribute("class", "t");
+
+  function count(month) {
+    let resultMonth;
+    switch (year) {
+      case 2022:
+        resultMonth = month + oldMonth - 12;
+        break;
+      case 2023:
+        resultMonth = month + 6;
+        break;
+      case 2024:
+        resultMonth = month + 12 + 6;
+
+        break;
+      case 2025:
+        resultMonth = month + 6 + 12 + 12;
+        break;
+
+      default:
+        resultMonth =
+          "Счетчик остановился полумуфта свое отработала(больше чем 3 года и дохрена)";
+    }
+    return resultMonth;
+  }
+  let r = count(month);
+  // let resultMonth = count(month);
+  //console.log(resultMonth, resultYear);
+
+  tel795.innerHTML = `Замена полумуфты на двигателе 9.06.22 прошло  ${r}месяцев `;
 }
