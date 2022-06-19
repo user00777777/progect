8c8d0942808862254fb3a161988c205e2b1779bd
user00777777 <misterborman@gmail.com>
2022-06-19 14:17:53 +0300

    change1

---
+++ repair.js
@@ -0,0 +1,6 @@
+let tel795 = document.getElementById("telf_795");
+tel795.addEventListener("click", telChange);
+function telChange() {
+  tel795.innerHTML = "Замена полумуфты на двигателе 9.06.22";
+  tel795.setAttribute("class", "t");
+}
