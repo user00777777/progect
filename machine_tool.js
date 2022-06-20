22b3645a7a6e9d5de25321801931202abb186d01
user00777777 <misterborman@gmail.com>
2022-06-20 21:32:36 +0300

    1

--- machine_tool.js
+++ machine_tool.js
@@ -2,9 +2,12 @@ let sb051 = document
   .getElementById("sb051")
   .addEventListener("click", Sbrepair);
 function Sbrepair() {
-  alert("d");
-}
-
-function SbRepair() {
-  alert("jhjhjh");
+  d0 = new Date(2022, 6, 15); //year month day
+  d1 = new Date();
+  dt = (d1.getTime() - d0.getTime()) / (1000 * 60 * 60 * 24);
+  document.write(
+    "<strong>" +
+      Math.round(dt) +
+      "</strong> - столько дне прошло с начала 2013-го года..."
+  );
 }
